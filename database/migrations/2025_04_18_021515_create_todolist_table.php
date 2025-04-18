9abce110925eb465d6503e4e1a8967e244937e0e
EVERRICH\33419 <jason_chang@everrich.com.tw>
2025-04-18 17:13:55 +0800

    feat: todo list 建置

---
+++ database/migrations/2025_04_18_021515_create_todolist_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodolistTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todolist', function (Blueprint $table) {
+            $table->id('id_todolist');
+            $table->tinyInteger('status')
+                ->comment('0: 未完成, 1:待確認, 2:已完成')
+                ->default(0);
+            $table->text('task_content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todolist');
+    }
+}
