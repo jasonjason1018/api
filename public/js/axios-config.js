a3d4061a237f7654694535e650579d7998dbbd09
EVERRICH\33419 <jason_chang@everrich.com.tw>
2025-04-18 17:13:55 +0800

    feat: todo list 建置

---
+++ public/js/axios-config.js
@@ -0,0 +1,25 @@
+const axiosInstance = axios.create({
+    baseURL: '/api',
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+});
+
+
+// axiosInstance.interceptors.request.use(config => {
+//     const token = localStorage.getItem('token'); // 或從 meta 抓 CSRF
+//     if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+// });
+
+axiosInstance.interceptors.response.use(
+    response => response,
+    error => {
+        console.error('API Error:', error.response?.data || error.message);
+        return Promise.reject(error);
+    }
+);
